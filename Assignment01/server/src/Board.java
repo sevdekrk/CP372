d6400c14d686ae5938c371918dc1bca43f2223ea
sevdekrk <sevdecimkorkut@gmail.com>
2026-02-05 11:41:35 -0500

    Update Board.java

--- Assignment01/server/src/Board.java
+++ Assignment01/server/src/Board.java
@@ -1,1 +1,11 @@
+public class Board {
+
+    public synchronized String postNote(...) { }
+    public synchronized String pin(...) { }
+    public synchronized String unpin(...) { }
+    public synchronized String shake() { }
+    public synchronized String clear() { }
+    public synchronized List<Note> getNotes(...) { }
+    public synchronized List<Pin> getPins() { }
+}
 
