02c7cabe71ba765c432abda56ec60c05d13e6ceb
sevdekrk <sevdecimkorkut@gmail.com>
2026-02-05 11:48:48 -0500

    Update Client.java

--- Assignment01/client/src/Client.java
+++ Assignment01/client/src/Client.java
@@ -1,6 +1,29 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+
     public static void main(String[] args) {
-        System.out.println("Client started...");
+        try (
+            Socket socket = new Socket("localhost", 5000);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(
+                socket.getOutputStream(), true);
+            Scanner scanner = new Scanner(System.in)
+        ) {
+            System.out.println(in.readLine());
+
+            while (true) {
+                System.out.print("> ");
+                String input = scanner.nextLine();
+                out.println(input);
+                System.out.println(in.readLine());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
-
