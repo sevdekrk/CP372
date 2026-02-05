4a051b4aef1ca2288a5082f55e0d9ea3d11c1e77
sevdekrk <sevdecimkorkut@gmail.com>
2026-02-04 21:09:34 -0500

    Delete server/src directory

--- server/src/client/src/Client.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-import java.net.*;
-
-public class Client {
-    public static void main(String[] args) {
-        System.out.println("Client started...");
-        // TODO: Connect to server, send commands, handle responses
-    }
-}
