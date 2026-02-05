91d66d632839d621569297932eb334ce988f62a9
sevdekrk <sevdecimkorkut@gmail.com>
2026-02-04 21:02:26 -0500

    Update Client.java

--- server/src/client/src/Client.java
+++ server/src/client/src/Client.java
@@ -1,5 +1,9 @@
+import java.io.*;
+import java.net.*;
+
 public class Client {
     public static void main(String[] args) {
-        System.out.println("Client starting...");
+        System.out.println("Client started...");
+        // TODO: Connect to server, send commands, handle responses
     }
 }
