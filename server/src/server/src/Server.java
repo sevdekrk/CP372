d63d337379d3bd46e5bee78ada44906a2ad91259
sevdekrk <sevdecimkorkut@gmail.com>
2026-02-04 21:02:52 -0500

    Update Server.java

--- server/src/server/src/Server.java
+++ server/src/server/src/Server.java
@@ -1,7 +1,10 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Server {
     public static void main(String[] args) {
-        System.out.println("Server starting...");
+        System.out.println("Server started...");
+        // TODO: Initialize board, accept clients, handle commands
     }
 }
-Add Server.java
-
