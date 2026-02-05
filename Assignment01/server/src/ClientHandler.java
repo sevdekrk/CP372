777acbb9b023cc69da976e0d9849e38c47c4da4d
sevdekrk <sevdecimkorkut@gmail.com>
2026-02-05 11:45:42 -0500

    Update ClientHandler.java

--- Assignment01/server/src/ClientHandler.java
+++ Assignment01/server/src/ClientHandler.java
@@ -1,1 +1,35 @@
+import java.io.*;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+
+    private final Socket socket;
+    private final Board board;
+
+    public ClientHandler(Socket socket, Board board) {
+        this.socket = socket;
+        this.board = board;
+    }
+
+    @Override
+    public void run() {
+        try (
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(
+                socket.getOutputStream(), true)
+        ) {
+            out.println("OK CONNECTED");
+
+            String line;
+            while ((line = in.readLine()) != null) {
+                String response = CommandProcessor.process(line, board);
+                out.println(response);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Client disconnected");
+        }
+    }
+}
 
