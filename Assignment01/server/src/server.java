5b6e1892573a9eed224ff4130322317f505d602a
sevdekrk <sevdecimkorkut@gmail.com>
2026-02-05 11:45:27 -0500

    Update server.java

--- Assignment01/server/src/server.java
+++ Assignment01/server/src/server.java
@@ -1,6 +1,35 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    public static final int PORT = 5000;
+    public static final int BOARD_WIDTH = 200;
+    public static final int BOARD_HEIGHT = 100;
+    public static final int NOTE_WIDTH = 20;
+    public static final int NOTE_HEIGHT = 10;
+
     public static void main(String[] args) {
-        System.out.println("Server started...");
+        Board board = new Board(
+                BOARD_WIDTH,
+                BOARD_HEIGHT,
+                NOTE_WIDTH,
+                NOTE_HEIGHT
+        );
+
+        System.out.println("Bulletin Board Server starting...");
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Listening on port " + PORT);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new Thread(new ClientHandler(socket, board)).start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
-
