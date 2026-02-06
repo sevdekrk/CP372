341b45de1763d364e27bcdc6bf32fd9c7fa31272
sevdekrk <sevdecimkorkut@gmail.com>
2026-02-05 19:02:58 -0500

    Update server.java

--- Assignment01/server/src/server.java
+++ Assignment01/server/src/server.java
@@ -2,35 +2,43 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+
 public class Server {
 
-   public static final int PORT = 6000;
 
-    public static final int BOARD_WIDTH = 200;
-    public static final int BOARD_HEIGHT = 100;
-    public static final int NOTE_WIDTH = 20;
-    public static final int NOTE_HEIGHT = 10;
+  public static final int PORT = 6000;
 
-    public static void main(String[] args) {
-        Board board = new Board(
-                BOARD_WIDTH,
-                BOARD_HEIGHT,
-                NOTE_WIDTH,
-                NOTE_HEIGHT
-        );
 
-        System.out.println("Bulletin Board Server starting...");
+   public static final int BOARD_WIDTH = 200;
+   public static final int BOARD_HEIGHT = 100;
+   public static final int NOTE_WIDTH = 20;
+   public static final int NOTE_HEIGHT = 10;
 
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-            System.out.println("Listening on port " + PORT);
 
-            while (true) {
-                Socket socket = serverSocket.accept();
-                new Thread(new ClientHandler(socket, board)).start();
-            }
+   public static void main(String[] args) {
+       Board board = new Board(
+               BOARD_WIDTH,
+               BOARD_HEIGHT,
+               NOTE_WIDTH,
+               NOTE_HEIGHT
+       );
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+
+       System.out.println("Bulletin Board Server starting...");
+
+
+       try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+           System.out.println("Listening on port " + PORT);
+
+
+           while (true) {
+               Socket socket = serverSocket.accept();
+               new Thread(new ClientHandler(socket, board)).start();
+           }
+
+
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+   }
 }
